56c41b939f647c0fff3af301b49aa204d8fe6d7d
jacquesuys <jaxuys@gmail.com>
2016-01-18 17:05:55 -0800

    Created ElaineDancer class.

---
+++ src/elaineDancer.js
@@ -0,0 +1,18 @@
+var ElaineDancer = function(top, left, timeBetweenSteps) {
+  Dancer.apply(this, arguments);
+
+  this.$node.css({
+    backgroundImage: 'url(elaine.gif)',
+    backgroundColor: 'transparent',
+    borderRadius: 0,
+    width: '100px',
+    height: '155px'
+  });
+};
+
+ElaineDancer.prototype = Object.create(Dancer.prototype);
+ElaineDancer.prototype.constructor = ElaineDancer;
+
+ElaineDancer.prototype.step = function() {
+  // BlinkyDancer.prototype.step.call(this, arguments);
+};
