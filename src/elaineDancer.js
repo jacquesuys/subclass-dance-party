483a37769970c2565fa23a747f37429d83c09ecb
jacquesuys <jaxuys@gmail.com>
2016-01-19 12:15:45 -0800

    convert Elaine to es6 and uncomment js files in html

--- src/elaineDancer.js
+++ src/elaineDancer.js
@@ -1,19 +1,27 @@
-var ElaineDancer = function(top, left, timeBetweenSteps) {
-  Dancer.apply(this, arguments);
+// var ElaineDancer = function(top, left, timeBetweenSteps) {
+//   Dancer.apply(this, arguments);
 
-  this.$node.css({
-    backgroundImage: 'url(elaine.gif)',
-    backgroundColor: 'transparent',
-    borderRadius: 0,
-    width: '100px',
-    height: '155px'
-  });
+  
+// };
+
+// ElaineDancer.prototype = Object.create(Dancer.prototype);
+// ElaineDancer.prototype.constructor = ElaineDancer;
+
+// ElaineDancer.prototype.step = function() {
+//   // BlinkyDancer.prototype.step.call(this, arguments);
+// };
+
+
+var ElaineDancer = class extends Dancer {
+  constructor (top, left, timeBetweenSteps) {
+    super(top, left, timeBetweenSteps);
+
+    this.$node.css({
+      backgroundImage: 'url(elaine.gif)',
+      backgroundColor: 'transparent',
+      borderRadius: 0,
+      width: '100px',
+      height: '155px'
+    });  
+  }
 };
-
-ElaineDancer.prototype = Object.create(Dancer.prototype);
-ElaineDancer.prototype.constructor = ElaineDancer;
-
-ElaineDancer.prototype.step = function() {
-  // BlinkyDancer.prototype.step.call(this, arguments);
-};
-
