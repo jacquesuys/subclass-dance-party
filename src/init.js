44b32bfa8650c83349875bda8e6dfc67d7445ad9
jacquesuys <jaxuys@gmail.com>
2016-01-18 16:43:51 -0800

    refactor so that square and blinky buttons are different shapes

--- src/init.js
+++ src/init.js
@@ -1,7 +1,23 @@
 $(document).ready(function() {
   window.dancers = [];
 
-  $(".addDancerButton").on("click", function(event) {
+  $(".addSquareDancer").on("click", function(event) {
+    var dancerMakerFunctionName = $(this).data("dancer-maker-function-name");
+    var dancerMakerFunction = window[dancerMakerFunctionName];
+
+    var dancer = new dancerMakerFunction(
+      $("body").height() * Math.random(),
+      $("body").width() * Math.random(),
+      Math.random() * 1000
+    );
+    dancer.$node.css({
+      backgroundColor: 'rgb('+ dancer.color[0] + ',' + dancer.color[1] + ',' + dancer.color[2] + ')',
+      borderRadius: 0
+    }); 
+    $('body').append(dancer.$node);
+  });
+
+   $(".addBlinkyDancer").on("click", function(event) {
     var dancerMakerFunctionName = $(this).data("dancer-maker-function-name");
     var dancerMakerFunction = window[dancerMakerFunctionName];
 
